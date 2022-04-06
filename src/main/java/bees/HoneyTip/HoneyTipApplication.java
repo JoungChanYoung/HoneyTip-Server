fc0a903b67aef06542019945b3a6e83e55dacbca
ChanYoung <cyjoungg@gmail.com>
2022-04-06 19:24:08 +0900

    first commit

---
+++ src/main/java/bees/HoneyTip/HoneyTipApplication.java
@@ -0,0 +1,13 @@
+package bees.HoneyTip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyTipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneyTipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bees/HoneyTip/HoneyTipApplicationTests.java
@@ -0,0 +1,13 @@
+package bees.HoneyTip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyTipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
